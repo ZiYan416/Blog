5fd03f8b0f3b0f79dc9a7c05d685c51589397d37
808 <Zi_Yan416@163.com>
2026-02-08 20:18:22 +0800

    [bug修复] 1. 修复了Github登录后无法正确获取用户信息的问题。

--- src/app/api/admin/users/[id]/route.ts
+++ src/app/api/admin/users/[id]/route.ts
@@ -3,8 +3,9 @@ import { NextResponse } from 'next/server'
 
 export async function DELETE(
   request: Request,
-  { params }: { params: { id: string } }
+  props: { params: Promise<{ id: string }> }
 ) {
+  const params = await props.params
   try {
     const supabase = await createServerClient()
 
